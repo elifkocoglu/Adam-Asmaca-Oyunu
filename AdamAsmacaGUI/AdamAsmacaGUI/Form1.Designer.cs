bd12861b794a3d925e74b0d3079932d671ee51c9
Elif Koçoğlu <147114190+elifkocoglu@users.noreply.github.com>
2025-01-12 23:30:13 +0300

    Add files via upload

---
+++ AdamAsmacaGUI/AdamAsmacaGUI/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace AdamAsmacaGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(436, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 36);
+            this.label1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(400, 200);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(400, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Tahmin Et";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(621, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 34);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Yanlış Tahminler\r\n:";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(32, 56);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(280, 349);
+            this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ AdamAsmacaGUI/AdamAsmacaGUI/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdamAsmacaGUI
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> turkiyeIlleri = new List<string>() { "Adana", "Adıyaman", "Afyonkarahisar", /* Diğer iller... */ };
+        string secilenIl;
+        char[] kelimeDurumu;
+        int yapilanHata = 0;
+        int maxHata = 6;
+        List<char> yanlisTahminler = new List<char>();
+        public Form1()
+        {
+            InitializeComponent();
+            OyunBaslat(); // Oyun başlatıldığında yeni bir kelime seç ve arayüzü hazırla
+            button1.Click += new EventHandler(button1_Click); // Butona tıklama olayını bağla
+        }
+        // Oyunu başlatan fonksiyon
+        private void OyunBaslat()
+        {
+            Random random = new Random();
+            secilenIl = turkiyeIlleri[random.Next(turkiyeIlleri.Count)].ToUpper(); // Rastgele bir il seç
+            kelimeDurumu = new string('_', secilenIl.Length).ToCharArray(); // Kelimenin durumunu "_" ile başlat
+            label1.Text = new string(kelimeDurumu);  // Kelime durumu ekrana yazılıyor
+            yanlisTahminler.Clear(); //yanlış tahmınleri sıfırla
+            label2.Text = "Yanlış Tahminler: ";  // Yanlış tahminler sıfırlanıyor
+
+            //hataları sıfırla
+            yapilanHata = 0;
+            panel1.Invalidate();  // Paneli temizle, adam çizimi sıfırlanacak
+        }
+
+        // "Tahmin Et" butonuna tıklanıldığında çalışacak
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 1) // Kullanıcının sadece bir harf girmesi gerekiyor
+            {
+                char tahmin = char.ToUpper(textBox1.Text[0]);
+                textBox1.Clear();
+
+                if (secilenIl.Contains(tahmin))
+                {
+                    // Doğru tahmin edilirse kelimeyi güncelle
+                    for (int i = 0; i < secilenIl.Length; i++)
+                    {
+                        if (secilenIl[i] == tahmin)
+                        {
+                            kelimeDurumu[i] = tahmin;
+                        }
+                    }
+                    label1.Text = new string(kelimeDurumu);  // Güncel kelime durumu ekrana yazılıyor
+                }
+                else
+                {
+                    // Yanlış tahmin edilirse
+                    if (!yanlisTahminler.Contains(tahmin))
+                    {
+                        yanlisTahminler.Add(tahmin);
+                        yapilanHata++;
+                        label2.Text = "Yanlış Tahminler: " + string.Join(", ", yanlisTahminler);
+                        panel1.Invalidate();  // Adam çizimini güncelle
+                    }
+                }
+
+                // Kazanma kontrolü
+                if (new string(kelimeDurumu) == secilenIl)
+                {
+                    MessageBox.Show("Tebrikler, kazandınız!");
+                    OyunBaslat();
+                }
+
+                // Kaybetme kontrolü
+                if (yapilanHata >= maxHata)
+                {
+                    MessageBox.Show("Kaybettiniz! Doğru cevap: " + secilenIl);
+                    OyunBaslat();
+                }
+            }
+        }
+
+        // Adam çizimini yapan fonksiyon
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.Black, 2);
+            Font font = new Font("Arial", 12);  // Gözler ve ağız için font
+
+            // Direği ve ipi çiz
+            g.DrawLine(pen, 10, 150, 90, 150);  // Zemini çiz
+            g.DrawLine(pen, 50, 20, 50, 150);   // Direği çiz
+            g.DrawLine(pen, 50, 20, 100, 20);   // Üst çubuğu çiz
+            g.DrawLine(pen, 85, 20, 85, 50);    // İpi biraz sağa kaydırarak çiz
+
+            // Adamı çiz
+            if (yapilanHata >= 1) g.DrawEllipse(pen, 70, 50, 30, 30);  // Kafa (ipi ortalayacak şekilde hizalandı)
+            if (yapilanHata >= 2) g.DrawLine(pen, 85, 80, 85, 130);    // Gövde
+            if (yapilanHata >= 3) g.DrawLine(pen, 85, 90, 65, 110);    // Sol kol
+            if (yapilanHata >= 4) g.DrawLine(pen, 85, 90, 105, 110);   // Sağ kol
+            if (yapilanHata >= 5) g.DrawLine(pen, 85, 130, 65, 160);   // Sol bacak
+            if (yapilanHata >= 6) g.DrawLine(pen, 85, 130, 105, 160);  // Sağ bacak
+        }
+
+     
+            private void button_Click_1(object sender, EventArgs e)
+            {
+                if (textBox1.Text.Length == 1) // Kullanıcının sadece bir harf girmesi gerekiyor
+                {
+                    char tahmin = char.ToUpper(textBox1.Text[0]); // Girilen harfi büyük harfe çevir
+                    textBox1.Clear(); // TextBox'ı temizle
+
+                    if (secilenIl.Contains(tahmin)) // Eğer doğru harfse
+                    {
+                        // Doğru tahmin edilirse kelimeyi güncelle
+                        for (int i = 0; i < secilenIl.Length; i++)
+                        {
+                            if (secilenIl[i] == tahmin)
+                            {
+                                kelimeDurumu[i] = tahmin;
+                            }
+                        }
+                        label1.Text = new string(kelimeDurumu);  // Güncel kelime durumu ekrana yazılıyor
+                    }
+                    else
+                    {
+                        // Yanlış tahmin edilirse
+                        if (!yanlisTahminler.Contains(tahmin))
+                        {
+                            yanlisTahminler.Add(tahmin);
+                            yapilanHata++;
+                            label2.Text = "Yanlış Tahminler: " + string.Join(", ", yanlisTahminler);
+                            panel1.Invalidate();  // Adam çizimini güncelle
+                        }
+                    }
+
+                    // Oyuncu kazandı mı kontrol et
+                    if (new string(kelimeDurumu) == secilenIl)
+                    {
+                        MessageBox.Show("Tebrikler, kazandınız!");
+                        OyunBaslat(); // Yeni oyun başlat
+                    }
+
+                    // Oyuncu kaybetti mi kontrol et
+                    if (yapilanHata >= maxHata)
+                    {
+                        MessageBox.Show("Kaybettiniz! Doğru cevap: " + secilenIl);
+                        OyunBaslat(); // Yeni oyun başlat
+                    }
+                }
+            }
+
+        }
+    }
+
